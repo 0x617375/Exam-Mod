b5dc2dbd4c63d37a3cec83625430643a78ce3a76
0x617375 <166357697+0x617375@users.noreply.github.com>
2025-10-12 17:42:09 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.exam.exammodwithx"
         minSdk = 21
         targetSdk = 32
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
         
         vectorDrawables { 
             useSupportLibrary = true
